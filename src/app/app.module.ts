1cb295f3226935a5a443b26c3dd70b21ef193f35
anishpdm <anish.vilayil.s@gmail.com>
2018-10-12 22:56:01 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { UserAddComponent } from './user-add/user-add.component';
+import { UserSearchComponent } from './user-search/user-search.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { HttpClientModule } from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserAddComponent,
+    UserSearchComponent,
+    NavBarComponent,
+    UserListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
